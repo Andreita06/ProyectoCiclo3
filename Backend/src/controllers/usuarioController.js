94f388bb1bd674141a0054df4d55387ad96fdda9
miguelmapa7 <miguel.perezar@cecar.edu.co>
2021-10-05 00:26:09 -0500

    Actualizando Controllers and Router de Usuarios

--- Backend/src/controllers/usuarioController.js
+++ Backend/src/controllers/usuarioController.js
@@ -1,37 +1,35 @@
 const usuario = require('../models/usuario');
 
-class UsuarioController{
+class UsuarioController {
 
-    constructor(){
-
+    constructor() {
     }
 
-    registrar(req, res){
-        
-        usuario.create(req.body, (error, data)=>{
-            if(error){
-                res.status(500).json({error});
-                
-            }else{
+    registrar(req, res) {
+
+        usuario.create(req.body, (error, data) => {
+            if (error) {
+                res.status(500).json({ error });
+            } else {
                 res.status(201).json(data);
             }
         });
     }
 
     //Método y ruta para consultar todos los usuarios
-    getUsuarios(req, res){
-        
-        usuario.find( (error, data)=>{
-            if(error){
-                res.status(500).json({error}); 
-            }else{
+    getUsuarios(req, res) {
+
+        usuario.find((error, data) => {
+            if (error) {
+                res.status(500).json({ error });
+            } else {
                 res.status(200).json(data);
-            }  
+            }
         });
     }
 
     //Update
-    setUsuario(req, res){
+    setUsuario(req, res) {
         //Capturar los datos del cuerpo de la petición
         let { identificacion, nombres, apellidos, direccion, correo, celular, clave } = req.body;
         //Crear un objeto con los datos capturados del cuerpo de la petición. Encargado de actualizar en el método update
@@ -39,23 +37,23 @@ class UsuarioController{
             identificacion, nombres, apellidos, direccion, correo, celular, clave
         }
         //Actualizar un usuario por ID
-        usuario.findByIdAndUpdate(id, {$set: objUsuario}, (error, data)=>{
-            if(error){
-                res.status(500).json({error});
-            }else{
+        usuario.findByIdAndUpdate(id, { $set: objUsuario }, (error, data) => {
+            if (error) {
+                res.status(500).json({ error });
+            } else {
                 res.status(200).json(data);
             }
         });
     }
 
-    deleteUsuario(req, res){
+    deleteUsuario(req, res) {
         let { id } = req.body;
-        
+
         //Eliminar un usuario por ID
-        usuario.findByIdAndRemove(id, (error, data)=>{
-            if(error){
-                res.status(500).json({error});
-            }else{
+        usuario.findByIdAndRemove(id, (error, data) => {
+            if (error) {
+                res.status(500).json({ error });
+            } else {
                 res.status(200).json(data);
             }
         });
---
+++ Backend/src/routers/usuarioRouter.js
@@ -0,0 +1,28 @@
+// Importando express
+const express = require('express');
+// Importando el controlador de los usuarios
+const UsuarioController = require('../controllers/usuarioController');
+
+class UsuarioRouter{
+
+    constructor(){
+        this.router = express.Router();
+
+        // Configurando las rutas
+        this.config();
+    }
+
+    config(){
+        // Creado objeto 
+        const objUsuarioController = new UsuarioController();
+
+        // Lo asignamos a la ruta
+        this.router.post("/usuario", objUsuarioController.registrar);
+        this.router.get("/usuario", objUsuarioController.getUsuarios);
+        this.router.put("/usuario", objUsuarioController.setUsuario);
+        this.router.delete("/usuario", objUsuarioController.deleteUsuario);
+    }
+
+}
+
+module.exports = UsuarioRouter;
