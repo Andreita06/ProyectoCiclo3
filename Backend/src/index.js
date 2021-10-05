7cfb9ecbcf9b7c13bd2b1c0c41a9bc345782d649
miguelmapa7 <miguel.perezar@cecar.edu.co>
2021-10-05 00:03:37 -0500

    Actualizando estructura y agregando rutas

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,20 +5,35 @@ const serverRouter = require('./routers/serverRouter');
 //Importar mongoose
 const mongoose = require('mongoose');
 //Importar url de conexión a la BD
-const database = require('./database/databaseConnection');
+const DatabaseConnection = require('./database/databaseConnection');
+// Importando otras rutas
+const AlertaController = require('./controllers/alertaController');
+const MascotaController = require('./controllers/mascotaController');
+const SesionController = require('./controllers/sesionController');
+const UsuarioController = require('./controllers/usuarioController');
+const VacunaController = require('./controllers/vacunaController');
 
 class Server {
     //constructor
     constructor() {
+        //Construir objeto de conexión a la BD
+        const dbConnection = new DatabaseConnection();
 
-        this.conectarBD();
+        // Crear app express
         this.app = express();
+
+        //Configurar parámetros de express para levantar el servidor
+        this.config();
+    }
+
+    config() {
 
         //Indicar el puerto por el que se ejecutará el servidor
         this.app.set('port', process.env.PORT || 3000);
 
         //Indicar que las solicitudes http se trabajará en JSON
         this.app.use(express.json());
+
         // Rutas
         const router = express.Router();
         router.get('/', (req, res) => {
@@ -26,24 +41,26 @@ class Server {
             res.status(200).json({ message: "Bienvenido to Pet Vaccine!" });
         });
 
+        // Creando rutas
         const serverR = new serverRouter.default();
+        const alertaController = new AlertaController();
+        const mascotaController = new MascotaController();
+        const sesionController = new SesionController();
+        const usuarioController = new UsuarioController();
+        const vacunaController = new VacunaController();
 
         //añadir las rutas al servidor
-        this.app.use(serverR.router);
         this.app.use(router);
+        this.app.use(serverR.router);
+        this.app.use(alertaController);
+        this.app.use(mascotaController);
+        this.app.use(sesionController);
+        this.app.use(usuarioController);
+        this.app.use(vacunaController);
 
         //Levantar el servidor/correr el servidor
         this.app.listen(this.app.get('port'), () => {
             console.log("Servidor corriendo por el puerto => ", this.app.get('port'));
-        });
-    }
-
-    conectarBD() {
-        mongoose.Promise = global.Promise;
-        mongoose.connect(database.db).then(() => {
-            console.log("Conexión a BD con éxito");
-        }).catch((err) => {
-            console.error("Error de conexión");
         });
     }
 }
