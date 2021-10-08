8d49d72ea3e9c196dc651aa7b48f63aea45eb595
FIRST_NAME LAST_NAME <sazlobo@gmail.com>
2021-10-07 21:43:10 -0500

    Se modifa el componente formulario y el Store

--- Frontend/ProyectoCiclo3/src/store/index.js
+++ Frontend/ProyectoCiclo3/src/store/index.js
@@ -5,14 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    usuarios: []
     // mascota: []
   },
   mutations: {
+    setUsuarios(state,payLoad){
+      state.usuarios = payLoad;
+    }
     // setMascota(state, payLoad) {
     //   state.mascota = payLoad;
     // }
   },
   actions: {
+    async cargarUsusarios({commit}){
+      const peticion = await fetch('http://localhost:3000/usuario');
+      const data = await peticion.json();
+      console.log(data);
+      commit('setUsuarios', data);
+    }
     // async getMascota({commit}){
     //   const peticion = await fetch('https://futuramaapi.herokuapp.com/api/v2/characters');
     //   const data = await peticion.json();
