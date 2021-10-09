a95f62c4bf4bdade20610342d0331bd8c7a70a6e
Andreita06 <andremar_06@hotmail.com>
2021-10-09 10:46:04 -0500

    Base de Datos conectada

--- Backend/src/database/urlDatabase.js
+++ Backend/src/database/urlDatabase.js
@@ -1,5 +1,5 @@
 //Exportamos la url de conexión a la BD
 module.exports = {
     //db: 'mongodb://localhost:27017/vacunasMascotas'
-    db: 'mongodb+srv://Andrea:Mascotas@mascotas.fx0pz.mongodb.net/Mascotas?retryWrites=true&w=majority'
+    db: 'mongodb://Andrea:Mascotas@mascotas-shard-00-00.fx0pz.mongodb.net:27017,mascotas-shard-00-01.fx0pz.mongodb.net:27017,mascotas-shard-00-02.fx0pz.mongodb.net:27017/ProyectoMascotas?ssl=true&replicaSet=atlas-12dpq1-shard-0&authSource=admin&retryWrites=true&w=majority'
 }
