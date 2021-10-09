e96d8103119ab1dbf1580708f82afd710ce74e73
miguelmapa7 <miguel.perezar@cecar.edu.co>
2021-10-09 08:32:24 -0500

    actualización de ruta de la bd remota

--- Backend/src/database/urlDatabase.js
+++ Backend/src/database/urlDatabase.js
@@ -1,4 +1,5 @@
 //Exportamos la url de conexión a la BD
 module.exports = {
-    db: 'mongodb://localhost:27017/vacunasMascotas'
+    // db: 'mongodb://localhost:27017/vacunasMascotas'
+    db: 'mongodb+srv://Andrea:<Mascotas>@mascotas.fx0pz.mongodb.net/Mascotas?retryWrites=true&w=majority'
 }
