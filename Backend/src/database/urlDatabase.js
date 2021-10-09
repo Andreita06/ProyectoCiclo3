b71dd8778c746ad4fec69bd0122ed740722a7cb8
miguelmapa7 <miguel.perezar@cecar.edu.co>
2021-10-09 11:14:26 -0500

    update

--- Backend/src/database/urlDatabase.js
+++ Backend/src/database/urlDatabase.js
@@ -1,10 +1,5 @@
 //Exportamos la url de conexión a la BD
 module.exports = {
-<<<<<<< HEAD
-    db: 'mongodb://localhost:27017/vacunasMascotas'
-    // db: 'mongodb+srv://Andrea:<Mascotas>@mascotas.fx0pz.mongodb.net/Mascotas?retryWrites=true&w=majority'
-=======
     //db: 'mongodb://localhost:27017/vacunasMascotas'
     db: 'mongodb://Andrea:Mascotas@mascotas-shard-00-00.fx0pz.mongodb.net:27017,mascotas-shard-00-01.fx0pz.mongodb.net:27017,mascotas-shard-00-02.fx0pz.mongodb.net:27017/ProyectoMascotas?ssl=true&replicaSet=atlas-12dpq1-shard-0&authSource=admin&retryWrites=true&w=majority'
->>>>>>> f76e034c8ebdf1f6d6a35fe124c36103aca4fceb
 }
