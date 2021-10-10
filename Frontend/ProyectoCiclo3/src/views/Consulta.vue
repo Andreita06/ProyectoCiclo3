36ffcdcf890e6c76560141745f3927835f621167
FIRST_NAME LAST_NAME <sazlobo@gmail.com>
2021-10-09 20:32:46 -0500

    cambio vista consulta

--- Frontend/ProyectoCiclo3/src/views/Consulta.vue
+++ Frontend/ProyectoCiclo3/src/views/Consulta.vue
@@ -1,55 +1,69 @@
 <template>
-  <div class="consulta">
-    <Navegacion></Navegacion> 
-    <v-app>
-      <div class="home">
-        <img alt="logo" src="../assets/MascotasVacunadas.jpg" />
-      </div>
-
+  <div class="container text-center">
+    <div class="container">
+    <img class="logo" alt="logo" src="../assets/MascotasVacunadas.jpg">
+    
+    
+    <v-card elevation="2" class="pa-5 ma-5" min-height="500px">
+      <h1 class="my-2 text-center">Mis mascotas</h1>
       <v-text-field
-        outlined
-        label="Buscador"
-        append-icon="mdi-magnify"
-        @click:append="consultar"
-      >
-      </v-text-field>
-
-      <v-card elevation="2" class="pa-5 ma-5" min-height="500px">
-        <v-row align="center" class="ma-0" justify="space between">
-          <Carta
-            nombreMascota="Maya"
-            ultimaFechaV="31/08/2021"
-            descripcion="Vacuna Séxtuple"
-          ></Carta>
-          <Carta
-            nombreMascota="Coco"
-            ultimaFechaV="23/12/2020"
-            descripcion="Vacuna triple viral"
-          ></Carta>
-          <Carta
-            nombreMascota="Violeta"
-            ultimaFechaV="07/10/2020"
-            descripcion="Vacuna Contra la Rabia"
-          ></Carta>
-        </v-row>
-      </v-card>
-      
-    </v-app>
+            outlined
+            label="Buscador"
+            append-icon="mdi-magnify"
+            @click:append="consultar"
+ >        
+    </v-text-field>
+      <v-row align="center" class="ma-0" justify="center">
+        <v-col
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3">
+          <Carta nombreMascota="Maya" ultimaFechaV="31/08/2021" descripcion="Vacuna Séxtuple"></Carta>
+        </v-col>
+        <v-col
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3">
+          <Carta nombreMascota="Maya" ultimaFechaV="31/08/2021" descripcion="Vacuna Séxtuple"></Carta>
+        </v-col>
+        <v-col
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3">
+          <Carta nombreMascota="Maya" ultimaFechaV="31/08/2021" descripcion="Vacuna Séxtuple"></Carta>
+        </v-col>
+        <v-col
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3">
+          <Carta nombreMascota="Maya" ultimaFechaV="31/08/2021" descripcion="Vacuna Séxtuple"></Carta>
+        </v-col>
+      </v-row>
+    </v-card>
+  </div>
   </div>
 </template>
 
 <script>
-
-import Navegacion from "../components/Navegacion.vue";
-import Carta from "../components/Carta.vue";
+//import Busqueda from '../components/Busqueda.vue'
+import Carta from '../components/Carta.vue'
 
 export default {
   name: "Consulta",
   components: {
-    Navegacion,
-    Carta,
+    //Busqueda,
+    Carta  
   },
-};
+  methods:{
+    consultar(){
+      console.log("consulta")
+    }
+  }
+}
 </script>
 
 <style lang="scss">
